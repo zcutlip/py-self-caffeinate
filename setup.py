b52aa243d2183e5c69e6b4717431c12b501bb3db
Zachary Cutlip <uid000@gmail.com>
2021-08-20 11:58:52 -0700

    make setup.py check platform==darwin

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+import sys
+
 from setuptools import setup
+
 about = {}
 with open("selfcaffeinate/__about__.py") as fp:
     exec(fp.read(), about)
 
 with open("README.md", "r") as fp:
     long_description = fp.read()
+
+
+def check_platform():
+    if sys.platform != 'darwin':
+        raise SystemExit("ERROR: requires macOS to install or build.")
+    return ''
+
 
 setup(name=about["__title__"],
       version=about["__version__"],
@@ -17,7 +27,7 @@ setup(name=about["__title__"],
       url="TBD",
       license="MIT",
       packages=['selfcaffeinate'],
-      python_requires='>=2.7',
-      install_requires=[],
+      python_requires='>=3.7',
+      install_requires=[check_platform()],
       package_data={'selfcaffeinate': ['config/*']},
       )
